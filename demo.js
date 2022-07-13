3927ce2e028b7a374ccf83759f78207366261177
madhuri605 <80457300+madhuri605@users.noreply.github.com>
2022-07-13 15:12:58 +0530

    add form and table

--- demo.js
+++ demo.js
@@ -1,6 +1,17 @@
-let arr=[1,2,3,4,5];
-let op=[];
-arr.forEach(element => {
-   op=element+1;
-})
-console.log(op);
+// let arr=[1,2,3,4,5];
+// let op=[];
+// arr.forEach(element => {
+//    op=element+1;
+// })
+// console.log(op);
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum());
+
+function sum(){
+    var a=10;
+}
+console.log(a);
